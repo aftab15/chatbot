6deda092e9c29b9e8872da42be7352244d4a609f
aftabpatwekar <aftab.patwekar@smarte.pro>
2024-03-28 17:11:58 +0530

    api call fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./App.css";
 import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
 import {
@@ -29,21 +29,42 @@ function App() {
     },
   ]);
   const [isTyping, setIsTyping] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const [selectedVersion, setSelectedVersion] = useState({
     label: "v1",
-    value: "https://dummyjson.com/products/",
+    value: "http://192.168.15.43:8087/getGPTApiResponse",
   });
 
   const options = [
     {
       label: "v1",
-      value: "https://dummyjson.com/products/",
+      value: "http://192.168.15.43:8087/getGPTApiResponse",
     },
     {
       label: "v2",
-      value: "https://dummyjson.com/products/",
+      value: "http://10.0.5.6:5000/api/ai_contact_discovery",
+    },
+    {
+      label: "v3",
+      value: "http://10.0.5.6:5000/api/buying_group_a1",
     },
   ];
+
+  var helpText;
+  switch (selectedVersion.label) {
+    case "v1":
+      helpText = <p className=" font-semibold">v1 prompt example: <span className=" font-normal select-text">what is the buying group for https://www.oracle.com/products at the https://www.smarteinc.com</span></p>
+      break;
+    case "v2":
+      helpText = <p className=" font-semibold">v2 prompt example: <span className=" font-normal select-text">find 10 contacts from accenture india</span></p>
+      break;
+    case "v3":
+      helpText = <p className=" font-semibold">v3 prompt example: <span className=" font-normal select-text">what is the buying group for https://www.oracle.com/products at the https://www.smarteinc.com</span></p>
+      break;
+    default:
+      helpText = <p className=" font-semibold">v1 prompt example: <span className=" font-normal select-text">what is the buying group for https://www.oracle.com/products at the https://www.smarteinc.com</span></p>
+      break;
+  }
 
   const handleSend = async (message) => {
     const newMessage = {
@@ -55,73 +76,70 @@ function App() {
     const newMessages = [...messages, newMessage];
     setMessages(newMessages);
     setIsTyping(true);
-    await processMessageToChatGPT(newMessages, message);
+    await processMessageToAPI(newMessages, message);
   };
 
-  async function processMessageToChatGPT(chatMessages, currentMessage) {
+  async function processMessageToAPI(chatMessages, currentMessage) {
+    let headers =
+      selectedVersion.label == "v1"
+        ? { "Content-Type": "application/json" }
+        : {};
+    setIsLoading(true);
     let data = {
       prompt: currentMessage,
     };
     var formData = new FormData();
-    formData.append("data", JSON.stringify(data));
-    await fetch(`${selectedVersion.value}${Math.floor(Math.random() * 10) + 1}`)
-      .then((res) => res.json())
+    if (selectedVersion.label == "v1") {
+      formData.append("data", JSON.stringify(data));
+    } else {
+      formData.append("prompt", currentMessage);
+    }
+    let reqBody =
+      selectedVersion.label == "v1" ? JSON.stringify(data) : formData;
+    await fetch(`${selectedVersion.value}`, {
+      headers: headers,
+      method: "POST",
+      body: reqBody,
+    })
+      .then((res) => {
+        if (res.status == 200) {
+          return res.json();
+        } else {
+          throw new Error("Failed to fetch data. Response code: " + res.status);
+        }
+      })
       .then((json) => {
         console.log(json);
         setMessages([
           ...chatMessages,
           {
-            message: json.title,
-            responseObject: [
-              {
-                Full_Name: "yesu hosmani",
-                Company_Name: "little flower of jesus high school",
-                Job_Title: "Student",
-                Contact_Location: "None",
-                Job_Started_On: "None",
-                Level: "Other",
-                Function: "Education / Teaching",
-                Contact_LinkedinUrl:
-                  "https://www.linkedin.com/in/yesu-hosmani-1b679b183",
-              },
-              {
-                Full_Name: "yesu hosmani",
-                Company_Name: "little flower of jesus high school",
-                Job_Title: "Student",
-                Contact_Location: "None",
-                "Job Started_On": "None",
-                Level: "Other",
-                Function: "Education / Teaching",
-                Contact_LinkedinUrl:
-                  "https://www.linkedin.com/in/yesu-hosmani-1b679b183",
-              },
-              {
-                Full_Name: "yesu hosmani",
-                Company_Name: "little flower of jesus high school",
-                Job_Title: "Student",
-                Contact_Location: "None",
-                "Job Started_On": "None",
-                Level: "Other",
-                Function: "Education / Teaching",
-                Contact_LinkedinUrl:
-                  "https://www.linkedin.com/in/yesu-hosmani-1b679b183",
-              },
-              {
-                Full_Name: "yesu hosmani",
-                Company_Name: "little flower of jesus high school",
-                Job_Title: "Student",
-                Contact_Location: "None",
-                "Job Started_On": "None",
-                Level: "Other",
-                Function: "Education / Teaching",
-                Contact_LinkedinUrl:
-                  "https://www.linkedin.com/in/yesu-hosmani-1b679b183",
-              },
-            ],
-            status: "200",
+            message: "Success",
+            responseObject:
+              selectedVersion.label == "v1"
+                ? json
+                : selectedVersion.label == "v2"
+                ? json.contacts
+                : json.buying_group,
+            status: 200,
             direction: "incoming",
           },
         ]);
+        setIsTyping(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setMessages([
+          ...chatMessages,
+          {
+            message: "Something went worng",
+            responseObject: [],
+            status: 500,
+            direction: "incoming",
+          },
+        ]);
+      })
+      .finally(() => {
+        setIsLoading(false);
         setIsTyping(false);
       });
   }
@@ -134,10 +152,10 @@ function App() {
             <Avatar src={Logo} name="Smarte AI" />
             <ConversationHeader.Content
               userName="Smarte AI"
-              info="We typically reply in a few minutes"
+              info={helpText}
             />
             <ConversationHeader.Actions>
-              <InfoButton className=" text-[#eb614b]"/>
+              <InfoButton className=" text-[#eb614b]" />
             </ConversationHeader.Actions>
           </ConversationHeader>
           <ChatContainer className="pt-16">
@@ -150,7 +168,7 @@ function App() {
               }
             >
               <MessageSeparator content={new Date().toDateString()} />
-              {messages.map((message, i) => {
+              {messages?.map((message, i) => {
                 return (
                   <Message key={i} model={message} position="single">
                     <Avatar src={Logo} />
@@ -166,6 +184,7 @@ function App() {
               onSend={(e) => {
                 handleSend(e);
               }}
+              disabled={isLoading}
             ></MessageInput>
           </ChatContainer>
           <Select
--- src/MessageCustomContent.jsx
+++ src/MessageCustomContent.jsx
@@ -8,88 +8,6 @@ const MessageCustomContent = ({ id, message }) => {
         message.direction !== "incoming") && <div>{message.message}</div>}
 
       {message.direction == "incoming" && id > 0 && message.status == "200" ? (
-        // <Card className="max-w-sm max-h-[300px] overflow-y-auto">
-        //   <div className="flow-root">
-        //     <ul className="divide-y divide-gray-200">
-        //       {message.responseObject.map((item, index) => {
-        //         return (
-        //           <li className="py-3 sm:py-3" key={index}>
-        //             <div className="flex flex-col gap-2">
-        //               <div className="min-w-0 flex-1 justify-start">
-        //                 <p className="truncate text-sm font-medium text-gray-900 text-left">
-        //                   Full Name
-        //                 </p>
-        //                 <p className="truncate text-sm text-gray-500 text-left">
-        //                   {item.Full_Name || "Not available"}
-        //                 </p>
-        //               </div>
-        //               <div className="min-w-0 flex-1 justify-start">
-        //                 <p className="truncate text-sm font-medium text-gray-900 text-left">
-        //                   Company Name
-        //                 </p>
-        //                 <p className="truncate text-sm text-gray-500 text-left">
-        //                   {item.Company_Name || "Not available"}
-        //                 </p>
-        //               </div>
-        //               <div className="min-w-0 flex-1 justify-start">
-        //                 <p className="truncate text-sm font-medium text-gray-900 text-left">
-        //                   Job Title
-        //                 </p>
-        //                 <p className="truncate text-sm text-gray-500 text-left">
-        //                   {item.Job_Title || "Not available"}
-        //                 </p>
-        //               </div>
-        //               <div className="min-w-0 flex-1 justify-start">
-        //                 <p className="truncate text-sm font-medium text-gray-900 text-left">
-        //                   Contact Location
-        //                 </p>
-        //                 <p className="truncate text-sm text-gray-500 text-left">
-        //                   {item.Contact_Location || "Not available"}
-        //                 </p>
-        //               </div>
-        //               <div className="min-w-0 flex-1 justify-start">
-        //                 <p className="truncate text-sm font-medium text-gray-900 text-left">
-        //                   Job Started On
-        //                 </p>
-        //                 <p className="truncate text-sm text-gray-500 text-left">
-        //                   {item.Job_Started_On || "Not available"}
-        //                 </p>
-        //               </div>
-        //               <div className="min-w-0 flex-1 justify-start">
-        //                 <p className="truncate text-sm font-medium text-gray-900 text-left">
-        //                   Level
-        //                 </p>
-        //                 <p className="truncate text-sm text-gray-500 text-left">
-        //                   {item.Level || "Not available"}
-        //                 </p>
-        //               </div>
-        //               <div className="min-w-0 flex-1 justify-start">
-        //                 <p className="truncate text-sm font-medium text-gray-900 text-left">
-        //                   Function
-        //                 </p>
-        //                 <p className="truncate text-sm text-gray-500 text-left">
-        //                   {item.Function || "Not available"}
-        //                 </p>
-        //               </div>
-        //               <div className="min-w-0 flex-1 justify-start">
-        //                 <p className="truncate text-sm font-medium text-gray-900 text-left">
-        //                   Contact Linkedin Url
-        //                 </p>
-        //                 <a
-        //                   href={item.Contact_LinkedinUrl}
-        //                   className="text-sm font-medium text-cyan-600 hover:underline text-left flex"
-        //                   target="_blank"
-        //                 >
-        //                 {item.Contact_LinkedinUrl || "Not available"}
-        //                 </a>
-        //               </div>
-        //             </div>
-        //           </li>
-        //         );
-        //       })}
-        //     </ul>
-        //   </div>
-        // </Card>
         <div className="overflow-x-auto">
           <Table>
             <Table.Head>
@@ -106,22 +24,22 @@ const MessageCustomContent = ({ id, message }) => {
               {message.responseObject.map((item, index) => {
                 return (
                   <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-                    <Table.Cell>{item.Full_Name || "Not available"}</Table.Cell>
+                    <Table.Cell>{item.Full_Name || "---"}</Table.Cell>
                     <Table.Cell>
-                      {item.Company_Name || "Not available"}
+                      {item.Company_Name || "---"}
                     </Table.Cell>
-                    <Table.Cell>{item.Job_Title || "Not available"}</Table.Cell>
+                    <Table.Cell>{item.Job_Title || "---"}</Table.Cell>
                     <Table.Cell>
-                      {item.Contact_Location || "Not available"}
+                      {item.Contact_Location || "---"}
                     </Table.Cell>
                     <Table.Cell>
-                      {item.Job_Started_On || "Not available"}
+                      {item.Job_Started_On || "---"}
                     </Table.Cell>
-                    <Table.Cell>{item.Level || "Not available"}</Table.Cell>
-                    <Table.Cell>{item.Function || "Not available"}</Table.Cell>
+                    <Table.Cell>{item.Level || "---"}</Table.Cell>
+                    <Table.Cell>{item.Function || "---"}</Table.Cell>
                     <Table.Cell>
                       <a href={item.Contact_LinkedinUrl} target="_blank">
-                      {item.Contact_LinkedinUrl || "Not available"}
+                      {item.Contact_LinkedinUrl || "---"}
                       </a>
                     </Table.Cell>
                   </Table.Row>
@@ -132,8 +50,8 @@ const MessageCustomContent = ({ id, message }) => {
         </div>
       ) : message.direction == "incoming" &&
         id > 0 &&
-        message.status != "200" ? (
-        <div>There are no contacts try modifying your prompt</div>
+        message.status != 200 ? (
+        <div>{message.message}</div>
       ) : null}
     </>
   );
